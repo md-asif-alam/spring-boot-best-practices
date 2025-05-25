0592f4b0bdcea87257a51ce4f3a68326d8bb46b1
Md Asif Alam <mdasifalam910@gmail.com>
2025-05-25 16:16:20 +0530

    pushed local project into remote

---
+++ spring-boot-restful-webservices/src/main/java/com/learnWithAsif/springboot/SpringBootRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.learnWithAsif.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ spring-boot-restful-webservices/src/test/java/com/learnWithAsif/springboot/SpringBootRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnWithAsif.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
